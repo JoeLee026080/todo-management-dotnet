cf85d6a0d9c1cc9a26bc42e9c4edda526dde422c
Joe <bj026080@gmail.com>
2026-03-15 00:08:21 +0800

    初始化 ASP.NET Core Todo API 專案

---
+++ TodoManagement.Api/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoManagement.Api.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "ok"
+        });
+    }
+}
